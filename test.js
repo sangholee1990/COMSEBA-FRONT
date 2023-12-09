885e9d8044a55dd0ca7a33531415e0ded0bd7430
Sang-Ho Lee <sangho.lee.1990@gmail.com>
2023-12-09 11:41:40 +0900

    commit

---
+++ test.js
@@ -0,0 +1,73 @@
+
+// 개발자도구에서 콘솔 확인
+console.log("Hello World2");
+
+// HTML 영역 확인
+document.write("Hello Wolrd3");
+
+// 경고창 메시지
+// alert("경고 메시지");
+
+// 변수 선언
+let msg = "Hellow Meg";
+// var msg = "Hellow Meg";
+
+console.log(msg);
+document.write(msg);
+// alert(msg);
+
+// 상수 선언
+const BIRTH_DAY = "1990.11.19";
+console.log(BIRTH_DAY);
+
+
+// 문제 1
+let admin = "John";
+let name = admin;
+// alert(admin);
+
+
+// 숫자형
+let num = 123;
+console.log(num);
+
+let num2 = 123.45;
+console.log(num2);
+
+// 문자형
+let str = "str";
+let str2 = 'single';
+console.log(str2);
+
+let phrase = `can another ${str}`;
+console.log(phrase);
+
+// 논리형
+let isCheck = true;
+console.log(isCheck);
+
+let isFlag = false;
+console.log(isFlag);
+
+let isGrater = 4 > 1;
+console.log(isGrater);
+
+// 변수 선언
+let age;
+console.log(age);
+
+// null 자료형 할당
+age = null;
+console.log(age);
+
+// 숫자형 할당
+age = 10;
+console.log(age);
+
+name = "Ilya";
+// console.log( `hello ${1}` );
+console.log( `hello 1` );
+// console.log( `hello ${"name"}` );
+console.log( `hello name` );
+
+console.log( `hello ${name}` );
